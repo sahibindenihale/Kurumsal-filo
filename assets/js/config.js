6ba195e8cadbd3f29f2b460c97a83cef591b7261
sahibindenihale <aviserent@gmail.com>
2026-03-17 21:01:03 +0300

    Update config.js
    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@ window.SITE_CONFIG = {
   companyName: 'GARENT KURUMSAL FİLO',
   phoneDisplay: '0501 592 61 05',
   phoneRaw: '905015926105',
-  whatsappRaw: '905015926105',
+  whatsappRaw: '905305147143',
   whatsappMessage: 'Merhaba, araç rezervasyonu yapmak istiyorum.',
   locations: ['İstanbul Havalimanı', 'Sabiha Gökçen', 'Taksim', 'Kadıköy', 'Beşiktaş', 'Bursa Merkez'],
   workingHours: '7/24 teslimat ve rezervasyon desteği'
