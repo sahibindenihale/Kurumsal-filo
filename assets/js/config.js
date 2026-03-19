c352a056fddb920b3f937f4bee74959f03b69cb4
sahibindenihale <aviserent@gmail.com>
2026-03-19 14:48:47 +0300

    Update config.js
    https://onesrentfilo.com/

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@ window.SITE_CONFIG = {
   companyName: 'GARENT KURUMSAL FİLO',
   phoneDisplay: '0501 687 42 34',
   phoneRaw: '905016874234',
-  whatsappRaw: '905016874234',
+  whatsappRaw: '905056772141',
   whatsappMessage: 'Merhaba, araç rezervasyonu yapmak istiyorum.',
   locations: ['İstanbul Havalimanı', 'Sabiha Gökçen', 'Taksim', 'Kadıköy', 'Beşiktaş', 'Bursa Merkez'],
   workingHours: '7/24 teslimat ve rezervasyon desteği'
